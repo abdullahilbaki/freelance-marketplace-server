8388d703c4f5fbe09228558d412a459955dd282a
abdullahilbaki <abdullahalbaqi00@gmail.com>
2025-06-24 12:10:06 +0600

    edited

--- firebase.js
+++ firebase.js
@@ -1,5 +1,7 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("./freelance-marketplace-service-account-key.json");
+const decoded = Buffer.from(process.env.FB_SERVICE_KEY, 'base64').toString('utf8');
+// const serviceAccount = require("./freelance-marketplace-service-account-key.json");
+const serviceAccount = JSON.parse(decoded);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
